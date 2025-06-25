81dcbe41ab8e0b772f063df6a9eb5c370d23fd03
Joaquin-Inofuentes <65864757+Joaquin-Inofuentes@users.noreply.github.com>
2025-06-25 17:55:23 -0300

    123123

--- Assets/Scripts/Nuevos de AM1/A3_H3_Monedas.cs
+++ Assets/Scripts/Nuevos de AM1/A3_H3_Monedas.cs
@@ -32,6 +32,7 @@ public class A3_H3_Monedas : A3_Interactuable
     }
     public void OnTriggerEnter(Collider other)
     { 
+        Debug.Log("Moneda recogida por: " + other.gameObject.name);
         AccionesJugador jugador = other.gameObject.GetComponent<AccionesJugador>();
         jugador.SumarMonedas(CantidadDeMonedas);
         jugador.Feedbacks.FeedbackRadialVisual(jugador.Color_ObtieneMonedas, 1f);
