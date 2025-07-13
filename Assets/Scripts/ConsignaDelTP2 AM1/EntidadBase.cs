462699597e5cd05bdf5d935d715e8323023fdb6a
Barzo22 <barzobruno11@gmail.com>
2025-07-13 03:52:09 -0300

    Lobito

--- Assets/Scripts/ConsignaDelTP2 AM1/EntidadBase.cs
+++ Assets/Scripts/ConsignaDelTP2 AM1/EntidadBase.cs
@@ -33,6 +33,24 @@ public abstract class EntidadBase : MonoBehaviour
         ultimaPosicion = transform.position;
         PingManager.RegistrarEntidad(this);
     }
+    void OnTriggerEnter(Collider other)
+    {
+        EntidadBase otraEntidad = other.GetComponent<EntidadBase>();
+        if (otraEntidad != null && otraEntidad != this && !entidadesCercanas.Contains(otraEntidad))
+        {
+            entidadesCercanas.Add(otraEntidad);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        EntidadBase otraEntidad = other.GetComponent<EntidadBase>();
+        if (otraEntidad != null && entidadesCercanas.Contains(otraEntidad))
+        {
+            entidadesCercanas.Remove(otraEntidad);
+        }
+    }
+
 
     public abstract void EjecutarCadaMedioSegundo();
 
