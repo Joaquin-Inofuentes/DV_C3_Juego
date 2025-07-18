0e8cc0de48c43a4e50becff6c59376ab9c3c3220
DamianDavincih <damian.figueredo@davinci.edu.ar>
2025-07-18 17:20:49 -0300

    supervivencia

--- Assets/generador.cs
+++ Assets/generador.cs
@@ -2,17 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class generador : MonoBehaviour
+public class GeneradorEnemigos : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject enemigoPrefab;    
+    public GameObject jefePrefab;       
+    public Transform[] puntosSpawn;     
+
+    public float tiempoEntreSpawns = 5f;
+    public int limiteEnemigos = 10;
+
+    private int enemigosCreados = 0;
+    private bool jefeGenerado = false;
+    private List<GameObject> enemigosActuales = new List<GameObject>();
+
     void Start()
     {
-        
+        StartCoroutine(GenerarEnemigos());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator GenerarEnemigos()
     {
-        
+        while (!jefeGenerado)
+        {
+            if (enemigosActuales.Count < limiteEnemigos)
+            {
+                CrearEnemigo();
+            }
+            else if (!jefeGenerado)
+            {
+                CrearJefe();
+                jefeGenerado = true;
+            }
+
+            yield return new WaitForSeconds(tiempoEntreSpawns);
+        }
+    }
+
+    void CrearEnemigo()
+    {
+        Transform punto = puntosSpawn[Random.Range(0, puntosSpawn.Length)];
+        GameObject enemigo = Instantiate(enemigoPrefab, punto.position, Quaternion.identity);
+        enemigosActuales.Add(enemigo);
+
+        enemigo.GetComponent<A1_A1_H1_MoustroDelAverno>().Morir += () => enemigosActuales.Remove(enemigo);
+
+        enemigosCreados++;
+    }
+
+    void CrearJefe()
+    {
+        Transform punto = puntosSpawn[Random.Range(0, puntosSpawn.Length)];
+        Instantiate(jefePrefab, punto.position, Quaternion.identity);
     }
 }
