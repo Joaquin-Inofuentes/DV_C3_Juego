f0b7d69a7a310dea671864d56c66d0c62e98f356
DamianDavincih <damian.figueredo@davinci.edu.ar>
2025-07-18 16:46:24 -0300

    bugs

--- Assets/Scripts/Nuevos de AM1/A2_H3_Charco.cs
+++ Assets/Scripts/Nuevos de AM1/A2_H3_Charco.cs
@@ -6,9 +6,29 @@ public class A2_H3_Charco : A2_Trampa
 {
     public GameObject EfectoEspecial;
     //public ATK_Congelar EfectoDeCongelado;
-    private void OnTriggerEnter(Collider other)
+    public override void OnTriggerEnter(Collider other)
     {
-        
+        if (!gameObject.activeSelf) return;
+        if (!other.gameObject.name.Contains("Jugador"))
+        {
+            A1_Entidad enemigo = other.gameObject.GetComponent<A1_Entidad>();
+            if (enemigo == null) return;
+
+            A1_A1_H1_MoustroDelAverno enemigo2 = enemigo.GetComponent<A1_A1_H1_MoustroDelAverno>();
+            if (enemigo2 != null && !enemigo2.Congelado)
+            {
+                GameObject efecto = Instantiate(EfectoEspecial, other.transform.position, Quaternion.identity);
+                enemigo2.Congelado = true;
+                enemigo2.PrimerAtaqueAAnular = true;
+
+                ATK_Congelar Componente = efecto.GetComponent<ATK_Congelar>();  // Ojo: cambiar EfectoEspecial por efecto
+                Componente.padre = other.transform;
+                Componente.agent = enemigo.agent;
+                Componente.anim = enemigo.anim;
+
+                Destroy(efecto, 1f);
+            }
+        }
     }
 
     public override void Activate()
@@ -26,25 +46,7 @@ public class A2_H3_Charco : A2_Trampa
 
     public override void OnCollisionEnter(Collision collision)
     {
-        if (!gameObject.activeSelf) return;
-        if(!collision.gameObject.name.Contains("Jugador"))
-        {
-
-        A1_Entidad enemigo = collision.gameObject.GetComponent<A1_Entidad>();
-        A1_A1_H1_MoustroDelAverno enemigo2 = enemigo.GetComponent<A1_A1_H1_MoustroDelAverno>();
-        if (enemigo2 != null && !enemigo2.Congelado)
-        {
-        GameObject efecto = Instantiate(EfectoEspecial, collision.transform.position, Quaternion.identity);
-            enemigo2.Congelado = true;
-            enemigo2.PrimerAtaqueAAnular = true;
-            ATK_Congelar Componente = EfectoEspecial.GetComponent<ATK_Congelar>();
-            Componente.padre = collision.transform;
-            Componente.agent = enemigo.agent;
-            Componente.anim = enemigo.anim;
-            Destroy(efecto, 1);
-
-        }
-        }
+        
     }
 
     protected override void Start()
