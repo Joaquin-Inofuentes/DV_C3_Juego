1234309baec9270c3229104187063e021cd6cef4
DamianDavincih <damian.figueredo@davinci.edu.ar>
2025-07-17 23:28:08 -0300

    tierra santa ok

--- Assets/Scripts/Nuevos de AM1/A2_H5_TierraSanta.cs
+++ Assets/Scripts/Nuevos de AM1/A2_H5_TierraSanta.cs
@@ -4,55 +4,64 @@ using UnityEngine;
 
 public class A2_H5_TierraSanta : A2_Trampa
 {
- 
-public class TierraSanta : MonoBehaviour
-{
-   private void OnTriggerEnter(Collider other)
-   {
+    public GameObject Indicadores1;
+    public GameObject Indicadores2;
+    public GameObject Indicadores3;
+    private void OnTriggerEnter(Collider other)
+    {
 
-            if (other.gameObject.name.Contains("Jugador 1"))
-            {
-                Debug.Log("Se registro entrada");
-            } 
-   }
-
-
-private void OnTriggerExit(Collider other)
-{
+        Debug.Log("1");
         AccionesJugador jugador = other.GetComponent<AccionesJugador>();
         if (jugador != null && jugador._TimerManager != null)
         {
-            jugador._TimerManager.magiaBloqueadaPorZona = false;
-            Debug.Log("Salió de Tierra Santa: magia desbloqueada");
+            jugador._TimerManager.magiaBloqueadaPorZona = true;
+            jugador._TimerManager.enModoMagico = false;
+            Indicadores1.SetActive(true); 
+            Indicadores2.SetActive(true);
+            Indicadores3.SetActive(true);
+
         }
-}
-}
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        AccionesJugador jugador = other.GetComponent<AccionesJugador>();
+        if (jugador != null && jugador._TimerManager != null)
+        {
+            jugador._TimerManager.magiaBloqueadaPorZona = false; 
+            Indicadores1.SetActive(false);
+        }
+    }
+
 
 public override void Activate()
-{
-    base.Activate();
+    {
+        base.Activate();
+    }
+
+    public override void Desactivar()
+    {
+        base.Desactivar();
+        // Agregar Logica de desactivar Sonido, Apariencia, Creacion etc
+    }
+
+    public override void OnCollisionEnter(Collision collider)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void Start()
+    {
+        base.Start(); // Llama al Start del padre
+                      // Código propio de ArquerasElfas
+    }
+
+    protected override void Update()
+    {
+        base.Update(); // Llama al Update del padre
+                       // Código propio de ArquerasElfas
+    }
 }
 
-public override void Desactivar()
-{
-    base.Desactivar();
-    // Agregar Logica de desactivar Sonido, Apariencia, Creacion etc
-}
-
-public override void OnCollisionEnter(Collision collider)
-{
-    throw new System.NotImplementedException();
-}
-
-protected override void Start()
-{
-    base.Start(); // Llama al Start del padre
-                  // Código propio de ArquerasElfas
-}
-
-protected override void Update()
-{
-    base.Update(); // Llama al Update del padre
-                   // Código propio de ArquerasElfas
-}
-}
+   
